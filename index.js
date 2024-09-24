330e0a02f5085d67494002f857859c6f80abce16
CMagnoPereira <126297569+CMagnoPereira@users.noreply.github.com>
2024-09-24 23:04:53 +0000

    24/Set/24

--- index.js
+++ index.js
@@ -20,27 +20,32 @@ console.log(`crase ${nomeCompleto}, estamos no ano ${anoAtual} e nasci no ano ${
 
 const ListaDeLocais = new Array(
   `Negrozaque`,
-  `Niggeria`,
-  `IceLandQueNaoTemIce`,
-  `GreemLandQueNaoTemGreem`,
-  `Italiland`
+  ` Niggeria`,
+  ` IceLandQueNaoTemIce`,
+  ` GreemLandQueNaoTemGreem`,
+  ` Italiland`
   );
 
 console.log(ListaDeLocais);
 
 console.log(ListaDeLocais[4]);
-ListaDeLocais. push("CoronelDesvivido");
+ListaDeLocais. push("Coronel Desvivido");
 console.log(ListaDeLocais);
 
 ListaDeLocais.splice(1,2);
 console.log(ListaDeLocais);
 
-var nomeComprador = entrada("Qual nome esta escrito em teu RG? ablublé")
-var idadeComprador = entrada("Qual sua idade?"); 
+var nomeComprador = entrada("Qual nome esta escrito em teu RG? ")
+var idadeComprador = entrada("Qual sua idade?  "); 
 
 if(idadeComprador <= 18){
-  console.log("Não.")
+  
+  console.log(`ola, ${nomeComprador} sua voce não possuia a idade minima ;para fazer essa compra. Apenas ${idadeComprador} anos de idade? patetico`)
 }
 if (idadeComprador >= 18){
-  console.log("Obrigado pelo o dinheiro")
+  console.log(`senhor(a) ${nomeComprador} seu dinheiro foi enviado com segunrança para o nosso bolso, agradeço sua compra`);
+
+ console.log(`Essas são nossa lista de locais para voce levar a sogra
+ 
+ ${ListaDeLocais}`)
 }
